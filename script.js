21f47b75d40004a6f0f563ce0c75905cd94813d7
fsbastidas <137218742+fsbastidas@users.noreply.github.com>
2025-02-16 09:48:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function updateButtonStyles(activeButton) {
 // Cargar datos de la base de datos
 async function loadDatabase(file) {
     try {
-        const response = await fetch(`BASES/${file}.json`);
+        const response = await fetch(`BASES/{file}.json`);
         if (!response.ok) throw new Error(`Error al cargar datos: ${response.status}`);
 
         const jsonData = await response.json();
